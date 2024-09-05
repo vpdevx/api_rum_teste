d901edeff85a7d701851193740bc9f382a9025e7
Vinicius Carvalho Pires <vinicpires03@gmail.com>
2024-09-05 17:59:10 -0300

    feat: Logging requests

--- src/main/java/com/br/clinica_api/ClinicaApiApplication.java
+++ src/main/java/com/br/clinica_api/ClinicaApiApplication.java
@@ -3,6 +3,7 @@ package com.br.clinica_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,4 +27,13 @@ public class ClinicaApiApplication {
         };
     }
 
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
 }
